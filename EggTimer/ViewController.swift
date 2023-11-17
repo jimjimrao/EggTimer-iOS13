b851539e6da5a95f3539264a0e67b94b47c9089d
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-17 17:43:46 -0500

    printing egg time depending on egg

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle?.lowercased()
+        let timeType: String = hardness! + "Time"
+        if timeType == "softTime" {
+            print(softTime)
+        }
+        else if timeType == "mediumTime" {
+            print(mediumTime)
+        }
+        else if timeType == "hardTime" {
+            print(hardTime)
+        }
+        
+    }
 }
