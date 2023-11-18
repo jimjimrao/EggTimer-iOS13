56d328a0156d1c7cd263723d35795e4108eae6b3
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-18 15:48:17 -0500

    countdown progressBar moves with timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -15,30 +15,42 @@ class ViewController: UIViewController {
         "Hard": 7
     ]
     var secondsRemaining = 60
+    var totalSeconds = 60
     
     var timer = Timer()
     
     @IBOutlet weak var MainText: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    override func viewDidLoad() {
+            super.viewDidLoad()
+            // Additional setup if needed
+        progressBar.progress = 1.0
+        }
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        progressBar.progress = 1.0
         
         timer.invalidate() // stops previous timer from continuing
         
         let hardness = sender.currentTitle!
         secondsRemaining = timeDict[hardness]!
-      
+        totalSeconds = secondsRemaining
+        
+        
         // re-initialize timer with new info
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         
         }
 
     @objc func updateTimer(){
+        progressBar.progress = (Float(secondsRemaining) / Float(totalSeconds))
         if secondsRemaining > 0 {
             MainText.text = String(secondsRemaining)
-            print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
         }
         else {
+            timer.invalidate()
             MainText.text = "Done!"
         }
     }
