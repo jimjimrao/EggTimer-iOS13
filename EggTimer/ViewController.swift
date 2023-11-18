02a25c2fc636509e39d4af420a6075cc09d931df
Jimmy Rao <32654532+jimjimrao@users.noreply.github.com>
2023-11-18 14:34:09 -0500

    add countdown function for when eggs are pressed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -14,11 +14,21 @@ class ViewController: UIViewController {
         "Medium": 420,
         "Hard": 720
     ]
+    func countdown(seconds: Int) {
+        var remainingSeconds = seconds
+        
+        while remainingSeconds > 0 {
+            print("\(remainingSeconds) seconds.")
+            remainingSeconds -= 1
+            sleep(1) // Pauses execution for 1 second
+        }
+    }
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle!
         let result = timeDict[hardness]!
         print(result)
+        countdown(seconds: result)
         
         }
         
